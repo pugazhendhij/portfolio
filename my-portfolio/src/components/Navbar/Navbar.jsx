8c2efb9b7c3abcb4f4c0a8ddc26ab023e175c92d
pugazhendhij <pugazhe9202@gmail.com>
2025-11-15 15:31:55 +0530

    first commit

---
+++ my-portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "../Navbar/navbar.css"
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
