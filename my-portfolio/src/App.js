97278a9f900dc28796ab8055f998c10a3cdc6946
pugazhendhij <pugazhe9202@gmail.com>
2025-11-16 23:14:06 +0530

    Revert changes

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,23 +1,26 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './components/Home/Home';
 import Navbar from "./components/Navbar/Navbar";
 import Contact from "./components/Contact/Contact";
 import Projects from "./components/Projects/Projects";
 import Resume from "./components/Resume/Resume";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </div>
+    
   );
 }
 
 export default App;
+
