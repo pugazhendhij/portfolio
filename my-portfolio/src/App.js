91f528e714b5784caff4b874b81c3605741e2a63
pugazhendhij <pugazhe9202@gmail.com>
2025-11-16 22:52:15 +0530

    portfolio updated

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/portfolo/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/contact" element={<Contact />} />
